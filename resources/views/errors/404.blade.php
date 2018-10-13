ba5e1df1211fd86954fc9ba6dc67c075a397d49c
mikeprosser <mprossersussex+github@gmail.com>
2018-10-13 16:36:14 +0100

    Add copy to home page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('page-title', 'Project NISEI - 404 Not Found')
+
+@section('content')
+
+    <div style="margin-top: 10rem;">
+        <h1 style="text-align: center;">404 - It doesn't look like there's anything here</h1>
+    </div>
+
+@stop
--- resources/views/partials/main-title.blade.php
+++ resources/views/partials/main-title.blade.php
@@ -7,3 +7,4 @@
         </div>
     </div>
 </section>
+<br>
