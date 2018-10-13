21e3b13333a409dd9e8944eea20da2665ae1dbda
mikeprosser <mprossersussex+github@gmail.com>
2018-10-13 15:50:18 +0100

    Set up node, sass and rebuild nav

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('page-title')</title>
+
+	<link rel="icon" type="image/png" href="/logo.png">
+
+	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="/css/app.css">	
+	<script src="/js/app.js"></script>
 </head>
 <body>
+	@include('partials.nav')
+
 	@yield('content')
 </body>
 </html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-md">
+    <div class="container">
+        <a class="navbar-brand" href="#">Project NISEI</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="navbar-hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/blog">Blog</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        About
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/about/netrunner">What is Netrunner?</a>
+                        <a class="dropdown-item" href="/about/nisei">Who or what is Project NISEI?</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Organised Play
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/op/for-stores">For retailers/TOs</a> 
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
