a8e5992b540d78cd18761f0015aa308f398a4fbc
mikeprosser <mprossersussex+github@gmail.com>
2018-10-13 18:30:33 +0100

    Add about/netrunner content

--- resources/views/pages/about/netrunner.blade.php
+++ resources/views/pages/about/netrunner.blade.php
@@ -7,4 +7,18 @@
     @include('partials.main-title', [
         'heading' => 'What is Netrunner?'
     ])
+
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-7">
+                    <p>Android: Netrunner is a two-player Living Card Game® set in a dystopian, cyberpunk future where monolithic megacorps own and control the vast majority of human interests.</p>
+                    <p>While corporation players try to score points by advancing their agendas, they have to guard their intellectual properties from the elite and subversive hackers known as netrunners. Netrunners attempt to win the game by stealing the corp's agendas, by any means possible!</p>
+                </div>
+                <div class="col-sm-12 col-lg-5">
+                    <img class="page-content__image" src="/assets/images/revised-core.png" alt="Android: Netrunner Revised Core Set">
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,13 +8,17 @@
         'heading' => 'Project NISEI'
     ])
 
-    <div class="container">
-        <div class="small-12">
-            <h4 class="headline"><span>What we do</span></h4>
-            <p>The Nextrunner International Support & Expansion Initiative (NISEI) is a fan-run organization to keep the game (Android: Netrunner) alive and thriving by establishing a new, non-FFG, means of supporting the player-base and creating content.</p>
-            <hr>
-            <h5>First time here here?</h5>
-            <p>Find out more about Android: Netrunner <a href="/about/netrunner">here</a>, and Project: NISEI <a href="/about/nisei">here.</a></p>
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="headline"><span>What we do</span></h4>
+                    <p>The Nextrunner International Support & Expansion Initiative (NISEI) is a fan-run organization to keep the game (Android: Netrunner) alive and thriving by establishing a new, non-FFG, means of supporting the player-base and creating content.</p>
+                    <hr>
+                    <h5>First time here here?</h5>
+                    <p>Find out more about Android: Netrunner <a href="/about/netrunner">here</a>, and Project: NISEI <a href="/about/nisei">here.</a></p>
+                </div>
+            </div>
         </div>
     </div>
 
