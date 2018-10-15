27542ea5872674b521b81817dca00e10e16abc4e
mikeprosser <mprossersussex+github@gmail.com>
2018-10-15 13:23:56 +0100

    Add viewport meta tag

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>@yield('page-title')</title>
+    <meta charset="UTF-8">
 
-	<link rel="icon" type="image/png" href="/logo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="/css/app.css">
-	<link rel="stylesheet" href="/css/app.css">	
-	<script src="/js/app.js"></script>
+    <title>@yield('page-title')</title>
+
+    <link rel="icon" type="image/png" href="/logo.png">
+
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/app.css">	
+    <script src="/js/app.js"></script>
 </head>
 <body>
-	@include('partials.nav')
+    @include('partials.nav')
 
-	@yield('content')
+    @yield('content')
 </body>
 </html>
