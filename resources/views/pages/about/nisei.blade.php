f2f9d82da62c61cf2cc61abf214a67c7b2bb1c02
mikeprosser <mprossersussex+github@gmail.com>
2018-10-24 21:43:55 +0100

    Format about nisei page

--- resources/views/pages/about/nisei.blade.php
+++ resources/views/pages/about/nisei.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-sm-12">
                     <p>The Nextrunner International Support & Expansion Initiative (NISEI) is a fan-run organization, with the mission to keep the game alive and thriving after the end of the official FFG era.</p>
                     <p>For this, we will be arranging prizes for casual game nights plus Store and Regional Championship competitive events, as well as arranging our own at the National and World Championship levels. In addition, we will be continuing the FFG tradition of updates to the rules and Most Wanted List, releasing new NISEI designed cards, and rotating the card pool to keep the meta environment fresh.</p>
-                    <p>Below you will find a list of the current NISEI board members, find us on stimhack.slack.com in the #NISEI channel, on Twitter as @ProjectNISEI or via email to projectNISEI@gmail.com if you want more information. </p>
+                    <p>Below you will find a list of the current NISEI board members, find us on <a href="https://stimhack.slack.com" target="_blank">stimhack.slack.com</a> in the #NISEI channel, on Twitter as <a href="https://twitter.com/projectnisei" target="_blank">@ProjectNISEI</a> or via email to <a href="mailTo:projectNISEI@gmail.com">projectNISEI@gmail.com</a> if you want more information. </p>
                 </div>
             </div>
         </div>
