434d4a5a3c32d7ec67d03e623e5357e143b1b6a6
mikeprosser <mprossersussex+github@gmail.com>
2018-10-25 23:00:22 +0100

    Restore titles to banner

--- resources/views/partials/main-title.blade.php
+++ resources/views/partials/main-title.blade.php
@@ -1,9 +1,9 @@
 <section class="main-title">
     <div class="container">
-        <!-- <div class="row">
+        <div class="row main-title__text-holder">
             <div class="col-sm-12">
                 <h1>{{ $heading }}</h1>
             </div>
-        </div> -->
+        </div>
     </div>
 </section>
