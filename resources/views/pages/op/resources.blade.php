db28b69ece0e934dde1c8c690d500f2bb3a6b840
Ian Witt <ianwittuk@gmail.com>
2018-12-11 10:20:44 +0000

    Added new resources to OP Resources page.

--- resources/views/pages/op/resources.blade.php
+++ resources/views/pages/op/resources.blade.php
@@ -12,7 +12,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <p>Coming soon: tournament brackets, decklists, nametag stickers, and more!</p>
+                    <p>Coming soon: tournament brackets, decklists, and more!</p>
                     <h2>For Players and Organizers</h2>
                     <ul>
                         <li><a href="/files/Comprehensive_Rules_1.1.pdf">NISEI Comprehensive Rules version 1.1</a></li>
@@ -20,6 +20,7 @@
                         <li><a href="/files/Organized_Play_Policies_1.0.pdf">Organized Play Policies</a></li>
                         <li><a href="/files/Formats_-_November_2018.pdf">Organized Play Formats</a></li>
                         <li><a href="/files/Easy_Rotation_Guide_-_Q4_2018.pdf">Easy Rotation Guide</a></li>
+                        <li><a href="/files/Nametag Stickers_-_14_Avery_7163.pdf">Nametag Stickers - 14 Avery 7163</a></li>                        
                     </ul>
                     <h2>For Organizers</h2>
                     <p>Note that the handwritten proxy templates are only intended for use by tournament staff. Handwritten proxies are not allowed in general. Please refer to the <a href="/files/Organized_Play_Policies_1.0.pdf">Organized Play Policies</a> for full details of the proxy policy.</p>
@@ -28,6 +29,8 @@
                         <li><a href="/files/Handwritten_Proxy_Template_%28Textless%29.pdf">Handwritten Proxy Template (Textless)</a></li>
                         <li><a href="/files/Q1_GNK_Poster_A4.png">Q1 GNK Poster (A4)</a></li>
                         <li><a href="/files/q1_gnk_poster_a3.png">Q1 GNK Poster (A3)</a></li>
+                        <li><a href="/files/Store_Championship_Poster_%28A4%29.png">Store Championship Poster (A4)</a></li>
+                        <li><a href="/files/Store_Championship_Poster_%28A3%29.png">Store Championship Poster (A3)</a></li>
                     </ul>
                 </div>
             </div>
