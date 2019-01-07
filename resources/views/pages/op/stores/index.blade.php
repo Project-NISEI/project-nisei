dc456be39a4940f87b07d466346aab1e9a028769
Ian Witt <ianwittuk@gmail.com>
2019-01-07 15:54:55 +0000

    Cleaned up handling of available/past kits

--- resources/views/pages/op/stores/index.blade.php
+++ resources/views/pages/op/stores/index.blade.php
@@ -18,33 +18,42 @@
             </div>
         </div>
         <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h4 class="headline"><span>Available Kits</span></h4>
-                    <br>
+            @php
+                $available = [];
+                $past = [];
+                list($available, $past) = $events->partition(function ($event) {
+                    return time() < strtotime($event->expires_on);
+                });
+            @endphp
+            @if ($available->count() > 0)
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4 class="headline"><span>Available Kits</span></h4>
+                        <br>
+                    </div>
+                    @foreach ($available as $event)
+                        @if (time() <= strtotime($event->expires_on))
+                            <div class="col-sm-12 col-lg-6">
+                                <a class="event-card" href="/op/available-kits/{{ $event->slug }}">
+                                    @if ($event->listing_image)
+                                        <img src="{{ $event->listing_image }}" alt="{{ $event->name }}" class="event-card__image">
+                                    @endif
+                                    <p class="event-card__name">{{ $event->name }}</p>
+                                    <p class="event-card__info">Base price: ${{ $event->price }}</p>
+                                    <p class="event-card__info">On sale until: {{ $event->expires_on }}</p>
+                                </a>
+                            </div>
+                        @endif
+                    @endforeach
                 </div>
-                @foreach ($events as $event)
-                    @if (time() <= strtotime($event->expires_on))
-                        <div class="col-sm-12 col-lg-6">
-                            <a class="event-card" href="/op/available-kits/{{ $event->slug }}">
-                                @if ($event->listing_image)
-                                    <img src="{{ $event->listing_image }}" alt="{{ $event->name }}" class="event-card__image">
-                                @endif
-                                <p class="event-card__name">{{ $event->name }}</p>
-                                <p class="event-card__info">Base price: ${{ $event->price }}</p>
-                                <p class="event-card__info">On sale until: {{ $event->expires_on }}</p>
-                            </a>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-            <br>
+                <br>
+            @endif
             <div class="row">
                 <div class="col-sm-12">
                     <h4 class="headline"><span>Past Kits</span></h4>
                     <br>
                 </div>
-                @foreach ($events as $event)
+                @foreach ($past as $event)
                     @if (time() > strtotime($event->expires_on))
                         <div class="col-sm-12 col-lg-6">
                             <a class="event-card" href="/op/available-kits/{{ $event->slug }}">
@@ -53,7 +62,7 @@
                                 @endif
                                 <p class="event-card__name">{{ $event->name }}</p>
                                 <p class="event-card__info">Base price: ${{ $event->price }}</p>
-                                <p class="event-card__info">On sale until: {{ $event->expires_on }}</p>
+                                <p class="event-card__subtle">Ended: {{ $event->expires_on }}</p>
                             </a>
                         </div>
                     @endif
