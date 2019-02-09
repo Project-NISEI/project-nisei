30c05bddf24ca011feefda656ec45f68dab2e8d1
Jen Pollock <jen@jenpollock.ca>
2019-02-09 12:40:25 -0700

    Add active tournament sets to OP menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 
+use App\TournamentSet;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+	View::share('active_tournament_sets', TournamentSet::getActive());
 
     }
 
