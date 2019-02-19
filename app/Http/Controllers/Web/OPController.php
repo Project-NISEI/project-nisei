e461b92ae67682bb1bb7b5857e76a078b299712a
Absotively <jen@jenpollock.ca>
2019-02-19 15:06:27 -0700

    Stop listing deleted events

--- app/Http/Controllers/Web/OPController.php
+++ app/Http/Controllers/Web/OPController.php
@@ -24,6 +24,7 @@ class OPController extends Controller
     {
         $events = DB::table('events')
             ->where('active', 1)
+            ->whereNull('deleted_at')
             ->select('name', 'slug', 'type', 'price', 'listing_image', 'expires_on')
             ->get();
 
