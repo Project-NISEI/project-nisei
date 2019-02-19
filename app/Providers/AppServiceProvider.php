4047475d37836cbb986befa74bdd025f51b80d50
Jen Pollock <jen@jenpollock.ca>
2019-02-18 17:19:47 -0700

    Re-remove unneeded changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\View;
 
-use App\TournamentSet;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
     }
 
     /**
