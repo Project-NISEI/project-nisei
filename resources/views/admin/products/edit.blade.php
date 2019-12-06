0f66fe1bd390bf606f4a0f16dba81af4074a4dbd
Jen Pollock <jen@jenpollock.ca>
2019-12-05 20:01:21 -0700

    Centralize CKEditor calling code, use CKEditor Enhanced Image plugin.
    This has the side effect of adding the CKEditor code to admin pages that don't need it, but we load so many third party js libraries on every admin page that I'm not sure it's going to be noticable.

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -85,18 +85,6 @@
 
 @section('javascript')
     @parent
-    <script src="//cdn.ckeditor.com/4.5.4/full/ckeditor.js"></script>
-    <script>
-        $('.editor').each(function () {
-                  CKEDITOR.replace($(this).attr('id'),{
-                    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-                    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{csrf_token()}}',
-                    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-                    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{csrf_token()}}'
-            });
-        });
-    </script>
-
     <script src="{{ url('adminlte/plugins/datetimepicker/moment-with-locales.min.js') }}"></script>
     <script src="{{ url('adminlte/plugins/datetimepicker/bootstrap-datetimepicker.min.js') }}"></script>
     <script>
