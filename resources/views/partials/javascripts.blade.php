1d1fcbe96f8591909a50524d8520fd79c98038e2
Jen Pollock <jen@jenpollock.ca>
2019-12-08 18:43:12 -0700

    Revert changes that shouldn't be in this branch.
    This reverts commit 0f66fe1bd390bf606f4a0f16dba81af4074a4dbd.

--- resources/views/partials/javascripts.blade.php
+++ resources/views/partials/javascripts.blade.php
@@ -39,18 +39,6 @@
      
 
 </script>
-<script src="//cdn.ckeditor.com/4.5.4/full-all/ckeditor.js"></script>
-<script>
-    $('.editor').each(function () {
-              CKEDITOR.replace($(this).attr('id'),{
-		extraPlugins: 'image2',
-                filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-                filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{csrf_token()}}',
-                filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-                filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{csrf_token()}}'
-        });
-    });
-</script>
 
 <script>
     $(function(){
